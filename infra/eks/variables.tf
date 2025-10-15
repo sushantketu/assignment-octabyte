68599bc38c07a0faebfe5c04ffea29881caa9d81
Sushant ketu <45790874+sushantketu@users.noreply.github.com>
2025-10-15 09:53:08 +0530

    Update variables.tf

--- infra/eks/variables.tf
+++ infra/eks/variables.tf
@@ -1,1 +1,21 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "key_name" {
+  description = "EC2 Key pair name for nodes"
+  type        = string
+}
 
