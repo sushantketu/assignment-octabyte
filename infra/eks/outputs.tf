2bf5c7435b5580ec5fd9b0f64477c7b4ad17198b
Sushant ketu <45790874+sushantketu@users.noreply.github.com>
2025-10-15 09:54:04 +0530

    Update outputs.tf

--- infra/eks/outputs.tf
+++ infra/eks/outputs.tf
@@ -1,1 +1,20 @@
 
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.sushantketu_vpc.id
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "EKS cluster security group ID"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "node_group_role_arn" {
+  description = "IAM role ARN for EKS worker nodes"
+  value       = module.eks.node_groups["sushantketu_nodes"].iam_role_arn
+}
